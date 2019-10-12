36661ed20c360b399b1e792564ffe50796755ce7
Ross Perkel <rossperkel@gmail.com>
2019-10-12 14:14:08 -0400

    got my formik hooked up start

--- src/components/LogASighting.js
+++ src/components/LogASighting.js
@@ -3,15 +3,40 @@ import { Formik } from 'formik'
 
 
 export default function LogASighting(){
-    return(<form>
-        <div className="input-row">
-            <input type="text"
-            name="name"
-            id="name"
-            />
-
-        </div>
-
-    </form>
+   
+    return(
+    <Formik initialValues={{name:"", bodega: ""}}>
+        {({values, errors, touched, handleChange, handleBlur}) => (
+                <form>
+                    {JSON.stringify(values)}
+                    <label htmlFor="name">Cat Name</label>
+                    <div className="input-row">
+                        <input type="text"
+                            name="name"
+                            id="name"
+                            placeholder="What's this kitty's name?"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.name}
+                        />
+                    </div>
+                    <label htmlFor="bodega">Bodega Name</label>
+                    <div className="input-row">
+                        <input type="text"
+                            name="bodega"
+                            id="bodega"
+                            placeholder="What's this kitty's bodega?"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.bodega}
+                        />
+                    </div>
+                    <div className="input-row">
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+        )} 
+    </Formik>
     )
+    
 }
