c9f99da837f4765cef9fba7909cea9f9abfbbfc8
Ross Perkel <rossperkel@gmail.com>
2019-10-14 11:30:17 -0400

    controlled forms with collection select for encounter creation

--- src/components/LogASighting.js
+++ src/components/LogASighting.js
@@ -5,20 +5,29 @@ import { Formik, Field } from 'formik'
 export default function LogASighting(props){
     
     console.log(props)
-    const bodega_id = props.bodega_id
+    const bodega_id = props.selectedBodega.id
     return(
         
-    <Formik initialValues={{cat_id:"", bodega_id: bodega_id }}>
+    <Formik initialValues={{cat_id: "", bodega_id: bodega_id, description: "", color: ""}}>
         {({values, errors, touched, handleChange, handleBlur}) => (
                 <form>
                     {JSON.stringify(values)}
-                    <label htmlFor="cat">Cat You Encountered</label>
-                    <Field component="select" name="color">
-                        {props.selectedBodega.attributes.cats.map((cat) => {
-                           return  <option value={cat.id}>{cat.name}</option>
-                        })}
-                    </Field>
-                    
+                    <div className="input-row">
+                        <label htmlFor="cat_id">Cat You Encountered</label>
+                        <Field
+                            component="select"
+                            name="cat_id"
+                            onChange={handleChange}
+                            >
+                                {props.selectedBodega.attributes.cats.map((cat) => {
+                                return  <option value={cat.id}>{cat.name}</option>
+                                })}
+                        </Field>
+                    </div>
+                    <div className="input-row">
+                        <label htmlFor="description">Description of your Encounter</label>
+                        <Field name="description" placeholder="Describe here!" />
+                    </div>
                     <div className="input-row">
                         <button type="submit">Submit</button>
                     </div>
