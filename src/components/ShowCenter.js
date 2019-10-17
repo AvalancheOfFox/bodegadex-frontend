4f206f2e557e66f76c5c60dcf3c7eb79c13a642c
Ross Perkel <rossperkel@gmail.com>
2019-10-17 11:56:15 -0400

    style changes, replaced scary crosshairs

--- src/components/ShowCenter.js
+++ src/components/ShowCenter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import crosshairs from "../svgs/crosshairs.svg";
+import catFace from "../svgs/cat-face-1024x1024.png"
 
 
 export default function ShowCenter(props){
@@ -7,7 +8,7 @@ export default function ShowCenter(props){
     // on hover, this component should highlight the center of the map so users can see where exactly they're targeting. 
     return(
         <div>
-           <img src={crosshairs} id="crosshairs" alt="Crosshairs overlaid on the map to show the user the geographic point they target when they create a new bodega point."/>
+           <img src={catFace} id="crosshairs" alt="Crosshairs overlaid on the map to show the user the geographic point they target when they create a new bodega point."/>
         </div>
     )
 
